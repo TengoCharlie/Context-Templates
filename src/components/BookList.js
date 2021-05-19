c125e06bb53a492dc956d3a6003f92fe074db7cb
Harsh Tenguriya <thegreatharsh1@gmail.com>
2021-05-19 06:35:03 +0530

    copy in booklist

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,21 +2,26 @@ import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 export class BookList extends Component {
-  static contextType = ThemeContext;
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <div
-        className="book-list"
-        style={{ background: theme.bg, color: theme.syntax }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>The way of Kings</li>
-          <li style={{ background: theme.ui }}>The name of the wionds</li>
-          <li style={{ background: theme.ui }}>The way to love</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div
+              className="book-list"
+              style={{ background: theme.bg, color: theme.syntax }}
+            >
+              <ul>
+                <li style={{ background: theme.ui }}>The way of Kings</li>
+                <li style={{ background: theme.ui }}>The name of the wionds</li>
+                <li style={{ background: theme.ui }}>The way to love</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
