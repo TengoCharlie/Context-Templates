e7fbc32deefc647fdd0a9c9ee944d4d46c058bb4
Harsh tenguriya <thegreatharsh1@gmail.com>
2021-05-21 18:05:37 +0530

    useState and Paasing on to another component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import NewSongForm from "./NewSongForm";
 
 export default function SongList() {
   const [songs, setSongs] = useState([
@@ -8,8 +9,8 @@ export default function SongList() {
     { title: "this wild darkness", id: 3 },
   ]);
 
-  const addSong = () => {
-    setSongs([...songs, { title: "new song", id: uuidv4() }]);
+  const addSong = (title) => {
+    setSongs([...songs, { title, id: uuidv4() }]);
   };
   return (
     <div className="song-list">
@@ -18,6 +19,7 @@ export default function SongList() {
           return <li key={song.id}>{song.title}</li>;
         })}
       </ul>
+      <NewSongForm addSong={addSong} />
       <button onClick={addSong}>Add a Song</button>
     </div>
   );
